2fd9ea91a24db3ce605d4ced406d52c4b2de7d7b
Thomas Ongeri <ongerit@gmail.com>
2016-10-10 15:10:41 -0400

    offset top by 60px

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -20,7 +20,7 @@ $(window).load(function() {
             // Using jQuery's animate() method to add smooth page scroll
             // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
             $('html,body').animate({
-                scrollTop: $(hash).offset().top
+                scrollTop: $(hash).offset().top + 60 //offset top by 60px
             }, 800, function(){
 
                 // Add hash (#) to URL when done scrolling (default click behavior)
@@ -127,7 +127,7 @@ $(window).load(function() {
 
     //Scroll reaveal
 
-    window.sr = ScrollReveal().reveal('section, .og__recommendations__item,  .og__profile ,.og__contact ');  // jshint ignore:line
+    window.sr = ScrollReveal().reveal('section, .og__recommendations__item, .og__profile , .og__contact ');  // jshint ignore:line
 
 
 });
