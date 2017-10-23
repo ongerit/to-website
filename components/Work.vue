47f2de2d2ee7c65724d843ccfb6d8616b15444ae
Thomas Ongeri <ongerit@gmail.com>
2017-10-23 14:43:18 -0400

    added work components

---
+++ components/Work.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="work">
+  </div>
+</template>
+
+<script>
+import BrandItem from './WorkItem'
+
+export default {
+  components: {
+    WorkItem
+  }
+}
+</script>
+
+<style lang='scss'>
+@import "../assets/styles/globals/grid";
+
+.work {
+  @extend %grid;
+  display: flex;
+  opacity: 0;
+
+  .loaded & {
+    opacity: 1;
+    transition: 2s opacity;
+  }
+}
+</style>
