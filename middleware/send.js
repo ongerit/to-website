d28bb2510addb890b705f16e8352b7d4bcd5f740
ongerit@gmail.com <ongerit@gmail.com>
2017-11-23 03:42:14 -0500

    remove sendgrid

--- middleware/send.js
+++ middleware/send.js
@@ -1,7 +1,7 @@
 /* eslint-disable */
 // using SendGrid's v3 Node.js Library
 // https://github.com/sendgrid/sendgrid-nodejs
-const sgMail = require('@sendgrid/mail');
+// const sgMail = require('@sendgrid/mail');
 sgMail.setApiKey(process.env.SENDGRID_API_KEY);
 
 export default function ({req, env, isServer}) {
