2663ebaaef11906669439babb47a456f54e7fd6a
ongerit@gmail.com <ongerit@gmail.com>
2018-06-25 01:28:55 -0400

    modified footer to include social media

--- components/Footer.vue
+++ components/Footer.vue
@@ -1,22 +1,46 @@
 <template>
-  <div class="footer">
-    <ul>
-      <li>Versions</li>
-    </ul>
-  </div>
+  <footer class="footer">
+    <div class="social">
+        <social-item class="icon-github" name='github' link="//www.bitly.com/GITOngeri"/>
+        <social-item class="icon-linkedin" name="linkedin" link="//www.bit.ly/thomas_ongeri"/>
+        <social-item class="icon-twitter" name='twitter' link="//www.bitly.com/TwitOngeri"/>
+        <social-item class="icon-instagram" name="instagram" link='//www.instagram.com/tomkins__'/>
+        <social-item class="icon-paypal" name="Palypal" link='//paypal.me/Ongeri'/>
+    </div>
+  </footer>
 </template>
-
 <script>
+import SocialItem from '~/components/SocialItem'
 
+export default {
+  components: {
+    SocialItem
+  }
+}
 </script>
 
 <style lang='scss'>
-.footer {
-  background: red;
-}
+@import "../assets/styles/utils/colors";
+@import "../assets/styles/globals/grid";
 
-li {
-  color: red;
-}
+  .footer {
+    @extend %grid;
+  }
+
+  .social {
+    padding: 50px 0;
+    width: 300px;
+    display: grid;
+    grid-gap: 10px;
+    grid-template-columns: repeat(5, 1fr);
+    justify-items: right;
+  
+    a {
+      font-size: 20px;
+      padding: 0;
+      text-decoration: none;
+    }
+  }
+
 
 </style>
