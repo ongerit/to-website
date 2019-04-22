aa3b4f6d10a50e12fb56ea151b0c1b925e2b6726
ongerit@gmail.com <ongerit@gmail.com>
2019-04-22 00:46:00 -0400

    version 2.xx

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,15 +1,13 @@
-require('dotenv').config()
-const bodyParser = require('body-parser')
+import pkg from './package'
 
-module.exports = {
-  plugins: [
-    { src: '~plugins/ga.js', ssr: false }
-  ],
+export default {
+  mode: 'universal',
+
   /*
   ** Headers of the page
   */
   head: {
-    title: 'Thomas Ongeri | thomasongeri.com',
+    title:'Thomas Ongeri | thomasongeri.com',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
@@ -20,45 +18,44 @@ module.exports = {
       { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico?v2' }
     ]
   },
+
   /*
-  ** Customize the progress bar color
+  ** Customize the progress-bar color
   */
   loading: { color: '#fe005c' },
-  /*
-  ** Build configuration
-  */
+
   serverMiddleware: [
     // body-parser middleware
     bodyParser.json()
   ],
+  /*
+  ** Global CSS
+  */
+  css: [
+  ],
+
+  /*
+  ** Plugins to load before mounting the App
+  */
+  plugins: [
+    { src: '~plugins/ga.js', ssr: false }
+  ],
+
+  /*
+  ** Nuxt.js modules
+  */
   modules: [
     '@nuxtjs/sitemap'
   ],
+
+  /*
+  ** Build configuration
+  */
   build: {
-    // vendor: ['axios'], // Add axios in the vendor.bundle.js
     /*
-    ** Run ESLint on save
+    ** You can extend webpack config here
     */
-    extend (config, ctx) {
-      if (ctx.dev && ctx.isClient) {
-        config.module.rules.push({
-          enforce: 'pre',
-          test: /\.(js|vue)$/,
-          loader: 'eslint-loader',
-          exclude: /(node_modules)/
-        })
-      }
-      const babelLoader = config.module.rules.find(
-        rule => rule.loader === 'babel-loader'
-      )
-      // babelLoader.exclude = /node_modules\/(?![@sendgrid])/
-      babelLoader.options.presets.push('es2015')
-      config.node = {
-        console: true,
-        fs: 'empty',
-        net: 'empty',
-        tls: 'empty'
-      }
+    extend(config, ctx) {
     }
   }
 }
