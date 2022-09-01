10383463e9565e3f6c3e72623ea7125a178ecfb1
ongerit@gmail.com <ongerit@gmail.com>
2022-09-01 17:26:48 -0500

    added new update to v2 of nuxt

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,64 +1,45 @@
-require('dotenv').config()
-const bodyParser = require('body-parser')
-
-module.exports = {
-  plugins: [
-    { src: '~plugins/ga.js', ssr: false }
-  ],
-  /*
-  ** Headers of the page
-  */
+export default {
+  // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    title: 'Thomas Ongeri | thomasongeri.com',
+    title: "Thomas Ongeri | thomasongeri.com",
+    htmlAttrs: {
+      lang: "en",
+    },
     meta: [
-      { charset: 'utf-8' },
-      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: 'Thomas Ongeri is a web developer currently residing in Brooklyn, New York.' },
-      { hid: 'keywords', name: 'keywords', content: 'spidermanfarfromhome.com, spiderman, far from home, vuejs, nuxt, javascript, tutorials, development, software, news, clients, work, contact, plotly, collegefashionista, dow jones, xfinity, maybelline, juilliard, aba, savage, nbc, natgeo, tough mudder, javascript, code, american express, express, ejs, webpack, react, lodash, node, go, drupal' }
+      { charset: "utf-8" },
+      { name: "viewport", content: "width=device-width, initial-scale=1" },
+      {
+        hid: "description",
+        name: "description",
+        content:
+          "Thomas Ongeri is a web developer currently residing in Brooklyn, New York.",
+      },
+      {
+        hid: "keywords",
+        name: "keywords",
+        content:
+          "spidermanfarfromhome.com, spiderman, far from home, vuejs, nuxt, javascript, tutorials, development, software, news, clients, work, contact, plotly, collegefashionista, dow jones, xfinity, maybelline, juilliard, aba, savage, nbc, natgeo, tough mudder, javascript, code, american express, express, ejs, webpack, react, lodash, node, go, drupal",
+      },
+      { name: "format-detection", content: "telephone=no" },
     ],
-    link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico?v2' }
-    ]
+    link: [{ rel: "icon", type: "image/x-icon", href: "/favicon.ico" }],
   },
-  /*
-  ** Customize the progress bar color
-  */
   loading: { color: '#fe005c' },
-  /*
-  ** Build configuration
-  */
-  serverMiddleware: [
-    // body-parser middleware
-    bodyParser.json()
-  ],
-  modules: [
-    '@nuxtjs/sitemap'
-  ],
-  build: {
-    // vendor: ['axios'], // Add axios in the vendor.bundle.js
-    /*
-    ** Run ESLint on save
-    */
-    extend (config, ctx) {
-      if (ctx.dev && ctx.isClient) {
-        config.module.rules.push({
-          enforce: 'pre',
-          test: /\.(js|vue)$/,
-          loader: 'eslint-loader',
-          exclude: /(node_modules)/
-        })
-      }
-      const babelLoader = config.module.rules.find(
-        rule => rule.loader === 'babel-loader'
-      )
-      // babelLoader.exclude = /node_modules\/(?![@sendgrid])/
-      babelLoader.options.presets.push('es2015')
-      config.node = {
-        console: true,
-        fs: 'empty',
-        net: 'empty',
-        tls: 'empty'
-      }
-    }
-  }
-}
+  // Global CSS: https://go.nuxtjs.dev/config-css
+  css: [],
+
+  // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
+  plugins: [],
+
+  // Auto import components: https://go.nuxtjs.dev/config-components
+  components: true,
+
+  // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
+  buildModules: [],
+
+  // Modules: https://go.nuxtjs.dev/config-modules
+  modules: [],
+
+  // Build Configuration: https://go.nuxtjs.dev/config-build
+  build: {},
+};
