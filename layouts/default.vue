cbcb164a92eef39af4ad1ce6ee6723e787ab2219
ongerit@gmail.com <ongerit@gmail.com>
2022-09-04 19:01:19 -0500

    update default

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,32 +1,29 @@
 <template>
-  <div>
-    <nuxt/>
-    <foot />
-  </div>
-</template>
-<script>
-import Foot from '~/components/Foot'
 
-export default {
-  components: {
-    Foot
-  }
-}
+  <div>
+
+    <nuxt />
+
+    <foot />
+
+  </div>
+
+</template>
+
+<script>
+ import Foot from '~/components/Foot' export default { components: { Foot } }
 </script>
 
 <script type="text/javascript">
-  (function (d, u, h, s) {
-    h = d.getElementsByTagName('head')[0];
-    s = d.createElement('script');
-    s.async = 1;
-    s.src = u + new Date().getTime();
-    h.appendChild(s);
-  })(document, 'https://grow.clearbitjs.com/api/pixel.js?v=');
+ if(typeof document !== 'undefined' ) { (function (d, u, h, s) { h = d.getElementsByTagName('head')[0];
+s = d.createElement('script'); s.async = 1; s.src = u + new Date().getTime(); h.appendChild(s);
+})(document, 'https://grow.clearbitjs.com/api/pixel.js?v='); }
 </script>
 
 <style>
 html {
-  font-family: "Montserrat", -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, "Helvetica Neue", Arial, sans-serif;
+  font-family: "Montserrat", -apple-system, BlinkMacSystemFont, "Segoe UI",
+    Roboto, "Helvetica Neue", Arial, sans-serif;
   font-size: 16px;
   word-spacing: 1px;
   -ms-text-size-adjust: 100%;
@@ -36,26 +33,29 @@ html {
   box-sizing: border-box;
 }
 
-*, *:before, *:after {
+*,
+*:before,
+*:after {
   box-sizing: border-box;
   margin: 0;
 }
 
 html {
-    box-sizing: border-box;
+  box-sizing: border-box;
 }
 
 body {
-    margin: 0;
-    font-family: "Montserrat", -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, "Helvetica Neue", Arial, sans-serif;
-    font-weight: 400;
-    -webkit-font-smoothing: antialiased;
+  margin: 0;
+  font-family: "Montserrat", -apple-system, BlinkMacSystemFont, "Segoe UI",
+    Roboto, "Helvetica Neue", Arial, sans-serif;
+  font-weight: 400;
+  -webkit-font-smoothing: antialiased;
 }
 
 *,
 *::before,
 *::after {
-    box-sizing: inherit;
+  box-sizing: inherit;
 }
 
 h1,
@@ -63,12 +63,12 @@ h2,
 h3,
 h4,
 h5 {
-    margin: 0;
-    padding: 0;
+  margin: 0;
+  padding: 0;
 }
 
 p {
-    margin: 0 0 20px;
+  margin: 0 0 20px;
 }
 
 ul,
@@ -76,34 +76,33 @@ ol,
 li,
 dl,
 dt,
-dd, {
-    margin: 0;
-    padding: 0;
-    list-style-type: none;
-    color: #fff;
+dd {
+  margin: 0;
+  padding: 0;
+  list-style-type: none;
+  color: #fff;
 }
 
 picture {
-    display: block;
+  display: block;
 }
 
 img {
-    display: block;
-    width: 100%;
-    height: auto;
+  display: block;
+  width: 100%;
+  height: auto;
 }
 
 a {
-    text-decoration: none;
-    cursor: pointer;
+  text-decoration: none;
+  cursor: pointer;
 
-    &:hover {
-        text-decoration: underline;
-    }
+  &:hover {
+    text-decoration: underline;
+  }
 }
 
 input::-ms-clear {
-    display: none;
+  display: none;
 }
-
 </style>
