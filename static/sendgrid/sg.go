6cc968496475562df9688b8a93569d4c592efd79
Thomas Ongeri <thomas@joinforma.com>
2023-07-04 14:40:41 -0500

    update go file

--- static/sendgrid/sg.go
+++ static/sendgrid/sg.go
@@ -51,7 +51,7 @@ func add_users_contact_to_sendgrid(apiKey string, user_email string, user_first_
 		]`, user_email, user_first_name, user_last_name)
 
 	request := sendgrid.GetRequest(apiKey, "/v3/contactdb/recipients", host)
-	request.Method = "PUT"
+	request.Method = "POST"
 	request.Body = []byte(body)
 	response, err := sendgrid.API(request)
 	if err != nil {
@@ -70,13 +70,43 @@ func add_users_contact_to_sendgrid(apiKey string, user_email string, user_first_
 // func send_transactional_email(apiKey, sgEmail, user_email, user_last_name, user_first_name, user_message){
 // }
 
+type RequestInfo struct {
+	IPAddress  string              `json:"ip_address"`
+	UserAgent  string              `json:"user_agent"`
+	HTTPMethod string              `json:"http_method"`
+	Headers    map[string][]string `json:"headers"`
+}
+
+
 func sendgridApi(w http.ResponseWriter, r *http.Request) {
 
+	
+	environment := os.Getenv("ENVIRONMENT")
+
 	// Set CORS headers
-	w.Header().Set("Access-Control-Allow-Origin", "*")
-	w.Header().Set("Access-Control-Allow-Methods", "GET, POST, OPTIONS")
-	w.Header().Set("Access-Control-Allow-Headers", "Content-Type")
-	w.Header().Set("Referrer-Policy", "no-referrer")
+	if environment != "prod" {
+		w.Header().Set("Access-Control-Allow-Origin", "*")
+		w.Header().Set("Access-Control-Allow-Methods", "GET, POST, OPTIONS")
+		w.Header().Set("Access-Control-Allow-Headers", "Content-Type")
+		w.Header().Set("Referrer-Policy", "no-referrer")
+	}
+
+	// Grab requester information
+
+	// Create a RequestInfo object
+	reqInfo := RequestInfo{
+		IPAddress:  r.RemoteAddr,
+		UserAgent:  r.Header.Get("User-Agent"),
+		HTTPMethod: r.Method,
+		Headers:    r.Header,
+	}
+
+	// Serialize RequestInfo as JSON
+	jsonData, err := json.Marshal(reqInfo)
+	if err != nil {
+		http.Error(w, "Failed to serialize request info", http.StatusInternalServerError)
+		return
+	}
 
 	if r.URL.Path != "/" {
 		http.Error(w, "404 not found.", http.StatusNotFound)
@@ -126,7 +156,7 @@ func sendgridApi(w http.ResponseWriter, r *http.Request) {
 		fmt.Fprintf(w, "LName = %s\n", user_last_name)
 		fmt.Fprintf(w, "Message = %s\n", user_message)
 
-		send_grid_information(apiKey, sgEmail, user_email, user_last_name, user_first_name, user_message)
+		send_grid_information(apiKey, sgEmail, user_email, user_last_name, user_first_name, user_message+"\n"+string(jsonData))
 		add_users_contact_to_sendgrid(apiKey, user_email, user_first_name, user_last_name)
 		//  send_email_with_template(apiKey, user_email, user_last_name, user_first_name, sgEmail)
 		//  send_transactional_email(apiKey, sgEmail, user_email, user_last_name, user_first_name, user_message)
