d07742b483c39912dcdbf8cefb699d725a9eed7b
Thomas Ongeri <thomas@superhuman.com>
2025-12-29 13:41:11 -0600

    fix version page

--- components/VersionItem.vue
+++ components/VersionItem.vue
@@ -1,14 +1,19 @@
 <template>
   <div class="version-item">
     <a :href='`//v${version}.thomasongeri.com`' :alt='title' target="_blank">
-      <img :src='`/ver/v${version}.png`' :title='title'/>
+      <img
+        :src='`/ver/v${version}.png`'
+        :title='title'
+        class="version-item__image"
+        style="width: 100%; height: auto; max-width: 400px;"
+      />
     </a>
     <div class="version-item__wrapper">
       <a :href='`//thomasongeri.com/v${version}`' :alt='title' target="_blank">
         <h3>{{title}} | v {{version}}.0</h3>
       </a>
       <ul>
-        <li v-for="item in tech">{{item}}</li>
+        <li v-for="item in tech" :key="item">{{item}}</li>
      </ul>
     </div>
   </div>
@@ -31,6 +36,17 @@ export default {
   border-radius: 1.5%;
   margin-bottom: 15px;
   display: inline-block;
+
+  .version-item__image {
+    display: block;
+    max-width: 100%;
+    width: 100%;
+    height: auto;
+    object-fit: contain;
+    margin: 0 auto;
+    background: $black-2;
+    border-radius: 4px;
+  }
 
   &__wrapper {
     padding: 15px;
@@ -68,10 +84,15 @@ export default {
     &:not(:last-child) {
       margin-right: 20px;
     }
+    .version-item__image {
+      max-width: 100%;
+      height: auto;
+    }
   }
 
   @include when-wider-than(small_desktop) {
-    img {
+    img,
+    .version-item__image {
       filter: invert(60%) grayscale(100%);
       transition: .5s filter;
 
@@ -79,6 +100,8 @@ export default {
         filter: invert(0) grayscale(0);
         transition: .5s filter;
       }
+      max-width: 100%;
+      height: auto;
     }
 
     margin-bottom: 40px;
@@ -91,6 +114,10 @@ export default {
     margin-bottom: 55px;
     &:not(:last-child) {
       margin-right: 50px;
+    }
+    .version-item__image {
+      max-width: 100%;
+      height: auto;
     }
   }
 }
--- pages/version.vue
+++ pages/version.vue
@@ -1,11 +1,23 @@
 <template>
   <section class="container">
-      <navigation />
-      <marquee title="Versions"/>
+    <Navigation />
+    <Marquee title="Versions" />
     <div class="version">
-      <version-item title="Catlett" version="3" v-bind:tech="['scss', 'vue', 'es6', 'nuxt', 'node', 'webpack', 'eslint', 'sendgrid', 'axios']"></version-item>
-      <version-item title="Basquiat" version="2"  v-bind:tech="['scss', 'vue',  'jquery', 'grunt', 'node', 'bower']"></version-item>
-      <version-item title="Alston" version="1"  v-bind:tech="['scss', 'jquery', 'grunt', 'node', 'bower']"></version-item>
+      <VersionItem
+        title="Catlett"
+        version="3"
+        :tech="['scss', 'vue', 'es6', 'nuxt', 'node', 'webpack', 'eslint', 'sendgrid', 'axios']"
+      />
+      <VersionItem
+        title="Basquiat"
+        version="2"
+        :tech="['scss', 'vue', 'jquery', 'grunt', 'node', 'bower']"
+      />
+      <VersionItem
+        title="Alston"
+        version="1"
+        :tech="['scss', 'jquery', 'grunt', 'node', 'bower']"
+      />
     </div>
   </section>
 </template>
@@ -23,11 +35,15 @@ export default {
   },
   methods: {
     animateElement () {
-      this.$el.classList.add('animated')
+      if (this.$el) {
+        this.$el.classList.add('animated')
+      }
       this.$nextTick(() => {
         setTimeout(() => {
           const $body = document.querySelector('body')
-          $body.classList.add('loaded')
+          if ($body) {
+            $body.classList.add('loaded')
+          }
         }, 1000)
       })
     }
@@ -39,26 +55,26 @@ export default {
 </script>
 
 <style lang="scss">
-  @import "../assets/styles/index";
-  @import "../assets/styles/globals/grid";
+@import "../assets/styles/index";
+@import "../assets/styles/globals/grid";
 
-  .container {
-    .marquee {
-      @extend %spacer;
-    }
+.container {
+  .marquee {
+    @extend %spacer;
+  }
+}
+
+.version {
+  @extend %grid;
+  opacity: 0;
+  transition: 2s opacity;
+  &.loaded, .loaded & {
+    opacity: 1;
   }
 
-  .version {
-    @extend %grid;
-    opacity: 0;
-    .loaded & {
-      opacity: 1;
-      transition: 2s opacity;
-    }
-
-    @include when-wider-than(tablet) {
-      display: flex;
-      justify-content: space-between;
-    }
+  @include when-wider-than(tablet) {
+    display: flex;
+    justify-content: space-between;
   }
+}
 </style>
